e324a93588cb33d224ee5bb1dad92bd87900585c
FatimaMahmud <13besefmahmud@seecs.edu.pk>
2016-05-25 01:56:09 +0500

    Add files via upload

--- Task1.scm
+++ Task1.scm
@@ -1,1 +1,27 @@
+#lang scheme
 
+(define aCompletelyUselessFunctionCreatedOnlyToMakeOurLivesMiserable
+  (λ (m n)
+    (cond
+      [(or (eq? m 0) (< m n))(myLoop m n)]
+      [else (+ m n)]
+      )
+    )
+  )
+
+(define myLoop
+  (λ (m n)
+    (myLoopRecursive 0 0 n)
+    )
+  )
+
+(define myLoopRecursive
+  (λ (sum i n)
+    (cond
+      [(< i n)(myLoopRecursive (+ sum i) (+ i 1) n)]
+      [else sum])
+    )
+  )
+
+(aCompletelyUselessFunctionCreatedOnlyToMakeOurLivesMiserable 0 10)
+(aCompletelyUselessFunctionCreatedOnlyToMakeOurLivesMiserable 10 7)
